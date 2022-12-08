108967a98f5e020e11644a505c518230f554afbb
xiaoleGun <1592501605@qq.com>
2022-12-08 08:59:51 +0800

    feat: Support auto install dependencies
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- build.sh
+++ build.sh
@@ -14,6 +14,24 @@ set -e
 BL=$(cd $(dirname $0);pwd)
 BD=$HOME/builds
 VERSION="0.6.0"
+ND=$(cd ../$(dirname $0);pwd)
+
+autoinstalldependencies() {
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+    id_like=$(awk -F= '$1 == "ID_LIKE" {print $2}' /etc/os-release)
+    if [[ "$distro" == "arch" || "$id_like" == "arch" ]]; then
+       echo "Arch Linux Detected"
+       git clone https://github.com/akhilnarang/scripts $BD/builds
+       cd $BD/scripts
+       bash setup/arch-manjaro.sh
+       cd $ND
+    else
+       sudo apt-get update
+       sudo apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev xattr openjdk-11-jdk jq android-sdk-libsparse-utils
+    fi
+fi
+}
 
 initrepo() {
 if [ ! -d .repo ]
@@ -190,6 +208,7 @@ personal() {
 START=`date +%s`
 BUILD_DATE="$(date +%Y%m%d)"
 
+autoinstalldependencies
 initrepo
 syncrepo
 applyingpatches
