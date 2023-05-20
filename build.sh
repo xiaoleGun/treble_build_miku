306484896072c88fe3313c36e3b5ea358b255205
xiaoleGun <1592501605@qq.com>
2023-05-20 15:38:37 +0800

    fixup! Auto install repo
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- build.sh
+++ build.sh
@@ -71,7 +71,7 @@ autoInstallDependencies() {
             echo "$password" | sudo -S apt-get update
             echo "$password" | sudo -S apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev xattr openjdk-11-jdk jq android-sdk-libsparse-utils python3 python -y
 
-            if [ ! -f /bin/repo -a -c `repo &>/dev/null` ];then
+            if [[ ! -f /bin/repo -a -c `repo &>/dev/null` ]]; then
                mkdir -p ~/bin
                curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
                echo "$password" | sudo -S cp ~/bin/repo /bin/repo
